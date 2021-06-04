65c703deb2e3001b150f0015a51263bbb35b97b3
Paul Masan <dev.rainingcloud@gmail.com>
2021-06-04 14:45:30 +0200

    Update EnviroObjectManager.lua

--- CommunityProjects/EnviroSystem/EnviroObjectManager.lua
+++ CommunityProjects/EnviroSystem/EnviroObjectManager.lua
@@ -9,6 +9,17 @@
 -- 
 --
 
+-- 
+-- 
+-- WinterResortSimulator Season 2
+-- Module EnviroObjectManager.lua
+-- 
+-- 
+-- Author:	Paul Masan (Paulchen)
+-- Date:	04/06/2021
+-- 
+--
+
 EnviroObjectManager = EnviroObjectManager or {};
 
 EnviroObjectManager.Objects = {};
@@ -17,26 +28,25 @@ EnviroObjectManager.UpdateCount = 15 * QualitySettings.getLODBias();
 
 function EnviroObjectManager.addObj(obj)
 	table.insert(EnviroObjectManager.Objects,#EnviroObjectManager.Objects + 1, obj);
+	addDestroyListener(obj.id, obj);
 end;
 
 function EnviroObjectManager.removeObj(obj)
-	for k, v in ipairs(EnviroObjectManager.Objects) do
+	for k, v in pairs(EnviroObjectManager.Objects) do
 		if v == obj then
 			table.remove(EnviroObjectManager.Objects,k);
-			return;
 		end;
 	end;
 end;
 
 function EnviroObjectManager:update(dt)
-	local shift = EnviroObjectManager.CurIndex;
 	local updateSize = (#EnviroObjectManager.Objects < EnviroObjectManager.UpdateCount and #EnviroObjectManager.Objects or EnviroObjectManager.UpdateCount);
-	for i = shift, updateSize, 1 do
-		if i > #EnviroObjectManager.Objects then shift = 1; end;
-		if EnviroObjectManager.Objects[shift] ~= nil then
-			EnviroObjectManager.Objects[shift]:update(dt);
+	for i = 1, updateSize, 1 do
+		if EnviroObjectManager.CurIndex > #EnviroObjectManager.Objects then EnviroObjectManager.CurIndex = 1; end;
+		if getIsTransformValid(EnviroObjectManager.Objects[EnviroObjectManager.CurIndex].id) then
+			EnviroObjectManager.Objects[EnviroObjectManager.CurIndex]:update(dt);
 		end;
-		shift = shift + 1;
+		EnviroObjectManager.CurIndex = EnviroObjectManager.CurIndex + 1;
 	end;
 end;
 addUpdateable(EnviroObjectManager);
